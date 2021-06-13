416fd081049194cfc8dda294e4f79b98631e6c7c
Keisuke Yanagisawa <yanagisawa@bi.c.titech.ac.jp>
2021-06-13 13:23:51 +0900

    revert

--- cmd_calculation/script/template_mdrun.sh
+++ cmd_calculation/script/template_mdrun.sh
@@ -12,21 +12,56 @@ fi
 ## for OpenMP parallelization
 export OMP_NUM_THREADS=$ncpus
 ## for thread-MPI parallelization
+#tMPI_COMM="-nt $ncpus"
 
-for name in {{ STEP_NAMES }}
+## minimize
+cd minimize
+
+for i in 1 2
 do
-    prev_cpt=""
     prev=$now
-    now=../simulation/$name
+    now=../minimize/min$i
 
     rm -f ${now}.out.mdp ${now}.tpr ${now}.log ${now}.gro ${now}.trr ${now}.edr ${now}.cpt
-    echo $GMX grompp -f ${now}.mdp -po ${now}.out.mdp -o ${now}.tpr \
+    $GMX grompp -f ${now}.mdp -po ${now}.out.mdp -o ${now}.tpr \
 	-c ${prev}.gro -p ${top_wo_ext}.top \
-	-r ${prev}.gro -n ../top/index.ndx ${prev_cpt}
-    echo $GMX mdrun -reprod -v -s ${now} -deffnm ${now} -cpi ${now}.cpt
-
-    prev_cpt="-t $now.cpt"
+	-r ${prev}.gro
+    $GMX mdrun -v -s ${now} -deffnm ${now} $tMPI_COMM     
 done
+cd ..
 
-# trr -> cpt
-# -t ${prev_cpt} trr? cpt?
+## heating - equillibration
+cd heat
+for i in 1 2 3 4 5 6 7 8 9
+do
+    prev=$now
+    if [ $i = 1 ];then
+	prev_cpt=$now.trr
+    else
+	prev_cpt=$now.cpt
+    fi
+    now=../heat/md$i
+
+    rm -f ${now}.out.mdp ${now}.tpr ${now}.log ${now}.gro ${now}.trr ${now}.edr ${now}.cpt
+    $GMX grompp -f ${now}.mdp -po ${now}.out.mdp -o ${now}.tpr \
+	-c ${prev}.gro -p ${top_wo_ext}.top \
+	-r ${prev}.gro -n ../top/index.ndx -t ${prev_cpt}
+    $GMX mdrun -v -s ${now} -deffnm ${now} -cpi ${now}.cpt $tMPI_COMM 
+done
+cd ..
+
+## production run
+cd pr
+prev=$now
+now=../pr/{{ NAME }}
+rm -f ${now}.out.mdp ${now}.tpr ${now}.log ${now}.gro ${now}.trr ${now}.edr ${now}.cpt
+$GMX grompp -f ${now}.mdp -po ${now}.out.mdp -o ${now}.tpr \
+    -c ${prev}.gro -p ${top_wo_ext}.top \
+    -r ${prev}.gro -n ../top/index.ndx -t ${prev}.cpt
+$GMX mdrun -v -s ${now} -deffnm ${now} -cpi ${now}.cpt $tMPI_COMM 
+cd ..
+
+## post calculation
+cd pr
+{{ POST_COMMAND }}
+cd ..
